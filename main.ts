a95370940b87b14857c0bb22675178ecfdc11936
hg054 <64025485+hg054@users.noreply.github.com>
2020-09-25 12:32:15 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,12 @@
 input.onLoudSound(function () {
+    input.setLoudSoundThreshold(150)
     light.setBrightness(20)
     light.showAnimation(light.rainbowAnimation, 200)
     makerController.player1.press(ArcadeButton.A)
     light.clear()
 })
 input.buttonB.onEvent(ButtonEvent.Click, function () {
+    music.pewPew.play()
     light.setBrightness(20)
     light.showAnimation(light.rainbowAnimation, 200)
     makerController.player1.press(ArcadeButton.B)
