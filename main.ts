4318a2bd1b678e4fcbe5d89215983b32b6c7172a
hg054 <64025485+hg054@users.noreply.github.com>
2020-09-25 10:35:52 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
-input.buttonA.onEvent(ButtonEvent.Click, function () {
+input.onLoudSound(function () {
+    input.setLoudSoundThreshold(100)
     light.setBrightness(20)
-    light.showAnimation(light.sparkleAnimation, 200)
+    light.showAnimation(light.cometAnimation, 200)
     makerController.player1.press(ArcadeButton.A)
     light.clear()
 })
